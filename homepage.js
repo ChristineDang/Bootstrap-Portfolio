a3ea55facd80ff683372268b62b769b64f658193
Christine Dang <dangc101@gmail.com>
2020-11-18 23:41:00 -0500

    linkings fixed. Parallax image adjustments

--- homepage.js
+++ homepage.js
@@ -1,19 +1,30 @@
 
-var images = [], x = -1;
-    images[0] = "";
-    images[1] = "";
-    images{2} = "";
+// var images = [], x = -1;
+//     images[0] = "";
+//     images[1] = "";
+//     images{2} = "";
 
-    function displayNextImage(){
-        x = (x === images.length - 1) ? 0 : x + 1;
-        document.getElementById("img").src = images[x];
-    }
+//     function displayNextImage(){
+//         x = (x === images.length - 1) ? 0 : x + 1;
+//         document.getElementById("img").src = images[x];
+//     }
 
-    function displayPrevImage(){
-        x = ( x <= 0) ? images.length - 1 : x - 1;
-        document.getElementById("img").src = images[x];
-    }
+//     function displayPrevImage(){
+//         x = ( x <= 0) ? images.length - 1 : x - 1;
+//         document.getElementById("img").src = images[x];
+//     }
 
-    function startTimer() {
-        setInterval;(displayNextImage, 3000);
-    }
+//     function startTimer() {
+//         setInterval;(displayNextImage, 3000);
+//     }
+
+var p = new Parallax('.parallax', {
+    offsetYBounds: 50,
+    intensity: 30,
+    center: 0.5,
+    safeHeight: 0.15
+  }).init()
+
+  p.on('element:loaded', function(element){
+    // do something with the element
+  })
